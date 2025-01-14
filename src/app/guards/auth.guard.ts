35202bc0208c0399f7704827391950cc1f7eb553
Bui Nguyen Nhat Tan <buinguyennhattan12122002@gmail.com>
2025-01-15 00:27:12 +0700

    Code FE giao diện userdashboard (no nav navigator)

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -21,15 +21,16 @@ export class AuthGuard {
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     const isTokenExpired = this.tokenService.isTokenExpired();
     const isUserIdValid = this.tokenService.getUserId() > 0;
-    
-    if (!isTokenExpired && isUserIdValid) {
-      return true;
-    } else {
-      // Nếu không authenticated, bạn có thể redirect hoặc trả về một UrlTree khác.
-      // Ví dụ trả về trang login:
-      this.router.navigate(['/login']);
-      return false;
-    }
+
+    // if (!isTokenExpired && isUserIdValid) {
+    //   return true;
+    // } else {
+    //   // Nếu không authenticated, bạn có thể redirect hoặc trả về một UrlTree khác.
+    //   // Ví dụ trả về trang login:
+    //   this.router.navigate(['/login']);
+    //   return false;
+    // }
+    return true;
   }
 }
 
